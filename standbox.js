0fb920ea12635b1a1adebf9b856f3cb3dbe98a68
Cauê Josê Oliveira Falcão <93996605+CaueJoseOliveiraFalcao@users.noreply.github.com>
2023-07-10 10:38:46 -0400

    js

---
+++ standbox.js
@@ -0,0 +1,7 @@
+const array = ['caue' , 'jose' , 'oliveira','falcao']
+
+const download = (array , title) => {
+    const textContent = array.join('\n')
+    console.log(textContent)
+}
+download(array,'mamaco')
